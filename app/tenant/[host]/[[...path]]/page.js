f990a6f1877c36810a6c1275ad50f084f57751dd
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2026-09-17 23:44:03 +0300

    Use redesigned CRTGO public menu

--- app/tenant/[host]/[[...path]]/page.js
+++ app/tenant/[host]/[[...path]]/page.js
@@ -1,70 +1,41 @@
-import {
-  notFound,
-} from "next/navigation";
+import { notFound } from "next/navigation";
 
-import {
-  getPublicProject,
-} from "@/services/menu/publicMenuData";
+import MenuWebsite from "@/components/public-site/MenuWebsite";
+import { getPublicProject } from "@/services/menu/publicMenuData";
 
-import StandardWebsite from "@/components/public-site/StandardWebsite";
-
-export async function generateMetadata({
-  params,
-}) {
-  const {
-    host,
-  } = await params;
-
-  const project =
-    await getPublicProject(host);
+export async function generateMetadata({ params }) {
+  const { host } = await params;
+  const project = await getPublicProject(host);
 
   if (!project) {
     return {
-      title: "CRTRGO",
+      title: "CRTGO",
     };
   }
 
   return {
     title: project.name,
-    description:
-      project.description ||
-      undefined,
-
-    icons:
-      project.faviconUrl
-        ? {
-            icon:
-              project.faviconUrl,
-          }
-        : undefined,
+    description: project.description || undefined,
+    icons: project.faviconUrl
+      ? {
+          icon: project.faviconUrl,
+        }
+      : undefined,
   };
 }
 
-export default async function TenantPage({
-  params,
-}) {
-  const {
-    host,
-    path,
-  } = await params;
+export default async function TenantPage({ params }) {
+  const { host, path } = await params;
 
-  if (
-    Array.isArray(path) &&
-    path.length > 0
-  ) {
+  if (Array.isArray(path) && path.length > 0) {
     notFound();
   }
 
-  const project =
-    await getPublicProject(host);
+  const project = await getPublicProject(host);
 
   if (!project) {
     notFound();
   }
 
-  return (
-    <StandardWebsite
-      website={project}
-    />
-  );
+  return <MenuWebsite website={project} />;
 }
